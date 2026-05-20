ab89346906193ee656d970bd8ca67b3291182339
Ange Marquise <angemarquisei@gmail.com>
2026-05-20 19:04:46 +0200

    added DAY 3

---
+++ DAY 3/operators.js
@@ -0,0 +1,28 @@
+// operators are symbols that perform operations on operands. They can be used to perform various tasks such as arithmetic, comparison, logical operations, and more. Here are some common types of operators in JavaScript:
+
+//1.mathematical operators
+// addition (+)
+//  subtraction (-) 
+// multiplication (*)
+//  division (/) 
+// modulus (%) 
+//exponentiation (**)
+
+//1. addtional operators:
+ let a = 10;
+ let b = 5;
+ let sum = a + b;
+console.log(sum); // Output: 15
+
+//2. subtraction operator:
+let difference = a - b;
+console.log(difference); // Output: 5   
+
+//3. multiplication operator:
+let product = a * b;
+console.log(product); // Output: 50
+
+//4. division operator: 
+let quotient = a / b;
+console.log(quotient); // Output: 2
+
