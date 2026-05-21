02b69f0b8bac5c5d2221bc3b6594c4dbaeb1a231
Ange Marquise <angemarquisei@gmail.com>
2026-05-21 11:23:43 +0200

    DAY 4

---
+++ DAY 4/LogicOperators.js
@@ -0,0 +1,50 @@
+
+let a = 5;
+let b = 10;
+let c=2;
+let d= "2";
+let password = "password123";
+
+
+//greater than >
+if (a > b) {
+    console.log("5 is greater than 10");
+} else
+    console.log("5 is not greater than 10");
+
+
+//less than <
+if (a < b) {
+    console.log("5 is less than 10");
+} else {
+    console.log("5 is not less than 10");
+}
+//greater than or equal to >=
+  if(password.length >= 8) {
+    console.log("Password is strong enough");
+  } else{
+    console.log("Password is too short");
+  }
+
+//less than or equal to <=
+if (a <= b) {
+    console.log("5 is less than or equal to 10");
+} else {
+    console.log("5 is not less than or equal to 10");
+}  
+
+//strict equality ===
+//first check the values and also the datatype:
+if (c === d) {
+    console.log("c is equal to d");
+}else { 
+    console.log("c is not equal to d");
+}
+
+//loose equality ==
+//only checks the values, not the datatype:
+if (c == d) {
+    console.log("c is  equal to d");
+}  else {
+    console.log("c is not equal to d");
+}
