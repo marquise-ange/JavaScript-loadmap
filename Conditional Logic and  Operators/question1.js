892164afc2e17817c061d6df5458a52372f9f42f
Ange Marquise <angemarquisei@gmail.com>
2026-05-23 18:02:42 +0200

    Exercises

---
+++ Conditional Logic and  Operators/question1.js
@@ -0,0 +1,8 @@
+let balance = 50000;
+let  withdrawAmount = 10000;
+ 
+if (withdrawAmount <= balance) {
+    console.log("withdrawal successful");
+} else {
+    console.log("insufficient funds");
+}
