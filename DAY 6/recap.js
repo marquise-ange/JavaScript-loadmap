28b6df19a405902efdd18288eb60c2912088ac68
Ange Marquise <angemarquisei@gmail.com>
2026-05-23 11:23:27 +0200

    day 6 recap

--- DAY 6/recap.js
+++ DAY 6/recap.js
@@ -43,4 +43,47 @@ switch (operator) {
         console.log("Invalid operator")
 }   
 
-// 
+// Atm machine
+let balance = 1000;
+let withdrawalAmount = 100;
+let checkBalance = 500;
+
+
+let 
+
+//  restaurant menu
+// 1 drink:water, juice, soda,milk
+// 2 fast food:burger, pizza, fries, hotdog
+// 3 coffee : espresso, latte, cappuccino, americano
+
+// order is greater than 50 discount
+
+let order= "drink";
+let price=0;
+
+switch (order) {
+    case"drink":
+    console.log("1.water, 2.juice, 3.soda,4.milk");
+    console.log("enter the number of the drink you want to order");
+    console.log("enter the quality");
+    console.log("enter");
+    break;
+
+    case"fast food":
+    console.log("1.burger, 2.pizza, 3.fries,4.hotdog");
+    console.log("enter the number of the fast food you want to order");
+    console.log("enter the quality");
+    console.log("total price is:");
+    break;
+
+     case"coffee":
+    console.log("1.espresso, 2.latte, 3.cappuccino,4.americano");
+    console.log("enter the number of the coffee you want to order");
+    console.log("enter the quality");
+    console.log("total price is:");
+    break;
+
+    default:
+        console.log("invalid order");
+}
+
