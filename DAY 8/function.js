0b94b05af4b15ad2b5d13dbfb9d330818f85402c
Ange Marquise <angemarquisei@gmail.com>
2026-05-28 14:44:07 +0200

    Day 8

---
+++ DAY 8/function.js
@@ -0,0 +1,2 @@
+//Functions in javaScript are a way to encapsulate a block of code that performs a specific task. They can be defined using the function keyword, followed by the function name and parentheses. The code to be executed is enclosed in curly braces {}.
+ function
